cc84d554d7527005ea733f25375c2fcc20723352
kaio eduardo <kaiocarvalhode@gmail.com>
2024-06-22 13:41:52 -0300

    atualizaçao

--- main.js
+++ main.js
@@ -1,233 +1,220 @@
-const { ipcRenderer, ipcMain } = require('electron')
-const { app, BrowserWindow, Menu } = require('electron/main')
+const { ipcMain, Menu, shell } = require('electron')
+const { app, BrowserWindow } = require('electron/main')
 const path = require('node:path')
 const { conectar, desconectar } = require('./database.js')
 
-// janela principal (definir o objeto win como variavel publica)
 let win
 const createWindow = () => {
-    win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        icon: './src/public/img/packge.png',
-        webPreferences: {
-        preload: path.join(__dirname, 'preload.js')
-        }
-    })
-    Menu.setApplicationMenu(Menu.buildFromTemplate(template))
-
-    win.loadFile('./src/views/index.html')
-}
-
-let about // bug de abertura
- 
-const aboutWindow = () => {      
-    // nativeTheme.themeSource ='dark'
-    // se a janela about noa etiver aberta
-    if (!about) {
-        about = new BrowserWindow({
-            width: 360,
-            height: 220,
-            icon: './src/public/img/packge.png',
-            resizable: false, // evitar o redimensionamento
-           // titleBarStyle: 'hidden',  esconder barra de titulo e menu
-          autoHideMenuBar: true // esconder menu
-        })
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: './src/public/img/',
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
     }
-    //iniciar a janela com o menu personalizado
-    about.loadFile('./src/views/sobre.html')
-    // bug2
-    about.on('closed', () => {
-        about = null
-    })
+  })
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+  win.loadFile('./src/views/index.html')
 }
 
+let about 
 
-let cliente // bug de abertura
+
+let clientes
+const clientWindow= () => {
+  // se a janela about não estiver aberta(bug 1)
+  if (!clientes) {
+    client = new BrowserWindow({
+      width: 360, //largura
+      height: 220,  //altura
+      resizable: false, //evitar o redimensionamento
+      autoHideMenuBar: true, //esconder menu
+      icon: './src/public/img/' //ícone
+    })
+  }
+
+  client.loadFile('./src/views/cliente.html')
  
-const clientes = () => {      
-    // nativeTheme.themeSource ='dark'
-    // se a janela about noa etiver aberta
-    if (!about) {
-        about = new BrowserWindow({
-            width: 360,
-            height: 220,
-            icon: './src/public/img/packge.png',
-            resizable: false, // evitar o redimensionamento
-           // titleBarStyle: 'hidden',  esconder barra de titulo e menu
-          autoHideMenuBar: true // esconder menu
-        })
-    }
-    //iniciar a janela com o menu personalizado
-    about.loadFile('./src/views/sobre.html')
-    // bug2
-    about.on('closed', () => {
-        about = null
-    })
-}
-
-
-let fornecedores // bug de abertura
  
-const fornecedor = () => {      
-    // nativeTheme.themeSource ='dark'
-    // se a janela about noa etiver aberta
-    if (!about) {
-        about = new BrowserWindow({
-            width: 360,
-            height: 220,
-            icon: './src/public/img/packge.png',
-            resizable: false, // evitar o redimensionamento
-           // titleBarStyle: 'hidden',  esconder barra de titulo e menu
-          autoHideMenuBar: true // esconder menu
-        })
-    }
-    //iniciar a janela com o menu personalizado
-    about.loadFile('./src/views/sobre.html')
-    // bug2
-    about.on('closed', () => {
-        about = null
-    })
+  client.on('closed', () => {
+    client = null
+  })
+
 }
 
-let produto // bug de abertura
- 
-const produtos = () => {      
-    // nativeTheme.themeSource ='dark'
-    // se a janela about noa etiver aberta
-    if (!about) {
-        about = new BrowserWindow({
-            width: 360,
-            height: 220,
-            icon: './src/public/img/packge.png',
-            resizable: false, // evitar o redimensionamento
-           // titleBarStyle: 'hidden',  esconder barra de titulo e menu
-          autoHideMenuBar: true // esconder menu
-        })
-    }
-    //iniciar a janela com o menu personalizado
-    about.loadFile('./src/views/sobre.html')
-    // bug2
-    about.on('closed', () => {
-        about = null
+let produtos
+const produtoWindow= () => {
+  
+  if (!produtos) {
+    produto = new BrowserWindow({
+      width: 360, 
+      height: 220, 
+      resizable: false, 
+      autoHideMenuBar: true, 
+      icon: './src/public/img/' 
     })
+  }
+
+  produto.loadFile("./src/views/produtos.html")
+
+  produtos.on('closed', () => {
+    produtos = null
+  })
+
+}
+
+const fornecWindow = () => {
+
+  if (!fornecedor) {
+    fornec = new BrowserWindow({
+      width: 360, 
+      height: 220,  
+      resizable: false, 
+      autoHideMenuBar: true, 
+      icon: './src/public/img/' 
+    })
+  }
+
+  fornecedor.loadFile('./src/views/fornecedor.html')
+  
+  forneceedor.on('closed', () => {
+    fornec = null
+  })
+
+}
+
+const aboutWindow = () => {
+  
+  if (!about) {
+    about = new BrowserWindow({
+      width: 360, 
+      height: 300,  
+      resizable: false, 
+      autoHideMenuBar: true, 
+      icon: './src/public/img/about.png' 
+    })
+  }
+
+  about.loadFile('./src/views/sobre.html')
+  
+  about.on('closed', () => {
+    about = null
+  })
+
 }
 
 
-
-
-
-
-// iniciar a aplicaçao 
 app.whenReady().then(() => {
-    //   status de conexao  com o banco de dados
-    ipcMain.on('send-message', (event, message) => {
-        console.log(`<<<${message}`)
-        statusConexao()
-    })
 
-    // desconectar do banco ao encerrar a janela 
-    app.on('before-quit', async () => {
-        await desconectar()
-    })
+  
+  ipcMain.on('send-message', (event, message) => {
+    console.log(`<<< ${message}`)
+    statusConexao()
+  })
 
-    createWindow()
+  
+  app.on('before-quit', async () => {
+    await desconectar()
+  })
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow()
-        }
-    })
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
 })
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
-
-
-//  Template do menu personalizado
 
 const template = [
-    {
-        label: 'Arquivo',
-        submenu: [
-            {
-                label: 'sair',
-                click: () => app.quit(),
-                accelerator: 'Alt+F4'
-            },
-            {
-               
-
-            }
-
-
-
-        ]
-    },
-    {
-        label: 'Exibir',
-        submenu: [
-            {
-                label: 'Recarregar',
-                role: 'reload'
-            }, {
-                label: 'Ferramentas do Desenvolvedor',
-                role: 'toggleDevTools'
-            }, {
-                type: 'separator'
-
-            }, {
-                label: 'aplicar zoom',
-                role: 'zoomIn'
-            }, {
-                label: 'Reduzir',
-                role: 'zoomOut'
-            }, {
-                label: 'Restaurar o zoom padrao',
-                role: 'resetZoom'
-            }
-        ]
-    },
-    {
-        label: 'Ajuda',
-        submenu: [
-            {
-                label: 'docs',
-                click: () => shell.openExternal('https://www.electronjs.org/docs/latest/')
-            },
-            {
-                type: 'separator'
-            },
-
-            {
-                label: 'Sobre',
-                accelerator: 'Alt+F1',
-                click: () => aboutWindow()
-            },
-        ]
-    }
-
+  {
+    label: 'Arquivo',
+    submenu: [
+      {
+        label: 'Clientes',
+        click: () => clientWindow(),
+      },
+      {
+        label: 'Fornecedores',
+        click: () => fornecWindow(),
+      },
+      {
+        label: 'Produtos',
+        click: () => produtoWindow(),
+      },
+      {
+        label: 'Sair',
+        click: () => app.quit(),
+        accelerator: 'Alt+F4'
+      }
+    ]
+  },
+  {
+    label: 'Exibir',
+    submenu: [
+      {
+        label: 'recarregar',
+        role: 'reload',
+      },
+      {
+        label: 'ferramentas do desenvolvedor',
+        role: "toggleDevTools",
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'aplicar zoom',
+        role: 'zoomIn',
+      },
+      {
+        label: 'Reduzir',
+        role: 'zoomOut',
+      },
+      {
+        label: 'restaurar o zoom padrão',
+        role: 'resetZoom',
+      }
+    ]
+  },
+  {
+    label: 'ajuda',
+    submenu: [
+      {
+        label: 'Sobre',
+        click: () => aboutWindow(),
+      }
+    ]
+  }
 ]
 
-
-
-// /-------------------------------------------------------------------
-// funcçao que verifica o status da conexao 
-const statusConexao = async () => {
-    try {
-        await conectar()
-        win.webContents.send('db-status', "banco de dados conectados.")
-    } catch (error) {
-        win.webContents.send('db-status', `erro de conexao${message}`)
-
-    }
-}
-
-// Exemplo 3 recebimento de uma açao a ser executada 
-
-ipcMain.on("open-about", () => {
-    aboutWindow()
+ipcMain.on('open-about', () => {
+  aboutWindow()
 })
+
+ipcMain.on('open-client', () => {
+  clientWindow()
+})
+
+ipcMain.on('open-fornec', () => {
+  fornecWindow()
+})
+
+ipcMain.on('open-produto', () => {
+  produtoWindow()
+})
+const statusConexao = async () => {
+  try {
+    await conectar()
+    win.webContents.send('db-status',"banco de dados conectado")
+  } catch (error) {
+    win.webContents.send('db-status', `eroo de conexao ${error.message}`)
+  }
+}
--- src/views/renderer.js
+++ src/views/renderer.js
@@ -1,14 +1,9 @@
-function sobre(){
-    api.openAbout()
- }
-
- // processo de renderizaçao   do documento index . html
-
-console.log("Processo  de Renderização")
-
-
-
-
-// exemplo de comando que so funciona no Node.js
-console.log(`Electron:${api.verElectron()}`)
-api.hello()
+function clientes() {
+    api.openclient()
+}
+function fornecedor() {
+    api.openfornecedor()
+}
+function produtos() {
+    api.openprodutos()
+}
