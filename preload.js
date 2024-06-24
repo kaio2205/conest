f041836801c60c9dc96a7d35e73ae97ef6eafc20
Kaio Eduardo <kaiocarvalhode@gmail.com>
2024-06-24 17:17:13 -0300

    atualizaçao

--- preload.js
+++ preload.js
@@ -1,9 +1,36 @@
-const { ipcRenderer } = require("electron")
+const { ipcRenderer, contextBridge } = require('electron')
 
-// status de conexao (verificar se obanco dedados esta conectado)
+// stt de conexão (verificar se o bd está conectado)
+contextBridge.exposeInMainWorld('api', {
+    openclient: () => ipcRenderer.send('open-client'),
+    openproduto: () => ipcRenderer.send('open-produto'),
+    openfornec: () => ipcRenderer.send('open-fornec')                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+})
 
-ipcRenderer.send('send-message',"Status do banco de dados:")
 
-ipcRenderer.on('db-status',(event,status)=>{
+
+
+
+
+ipcRenderer.send('send-message', "Status do banco de dados:")
+
+ipcRenderer.on('db-status', (event, status) => {
     console.log(status)
+})
+
+// inserir data na página
+function obterData() {
+    const data = new Date()
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }
+    return data.toLocaleDateString('pt-BR', options)
+}
+
+// interagir diretamente no DOM do documento html (index.html)
+window.addEventListener('DOMContentLoaded', () => {
+    const dataAtual = document.getElementById('dataAtual').innerHTML = obterData()
 })
